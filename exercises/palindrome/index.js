330c261d54f65a49d0ddf9d0c69e5f88439465df
passakun <passakun@thinknet.co.th>
2018-02-21 12:46:40 +0700

    palindrome exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,10 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const reversedstr = str.split('').reverse().join('');
+    console.log('reversed str', reversedstr);
+    return str === reversedstr;
+}
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,6 +7,17 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    return str.split('').reverse().join('');
+    return str.split('').reduce((rev, char) => char + rev )
 }
 module.exports = reverse;
+
+// return str.split('').reverse().join('');
+
+// function reverse(str) {
+//     let reversed = '';
+//     for( let character of str) {
+//         reversed = character + reversed;
+//     }
+//     console.log('reversed', reversed)
+//     return reversed;
+// }
